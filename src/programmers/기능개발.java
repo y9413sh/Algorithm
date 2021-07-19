7c2a03761d531b3b8fdf12e3198ea7ec219ee293
Yee_Dev <y9413sh@gmail.com>
2021-07-20 06:43:02 +0900

    Create 기능개발.java

---
+++ src/programmers/기능개발.java
@@ -0,0 +1,19 @@
+package src.programmers;
+
+// 문제 출처: 프로그래머스
+// URL: https://programmers.co.kr/learn/courses/30/lessons/42586
+
+public class 기능개발 {
+
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+
+
+        
+        return answer;
+    }
+    
+    public static void main(String ars[]) {
+
+    }
+}
