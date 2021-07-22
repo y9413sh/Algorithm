4d67e996beb732ac23306aa22efb0fbd98b49d20
Yee_Dev <y9413sh@gmail.com>
2021-07-23 06:27:51 +0900

    Update 기능개발.java
    문제풀이

--- src/programmers/기능개발.java
+++ src/programmers/기능개발.java
@@ -1,16 +1,34 @@
 package src.programmers;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 // 문제 출처: 프로그래머스
 // URL: https://programmers.co.kr/learn/courses/30/lessons/42586
 
 public class 기능개발 {
 
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
+        Queue<Integer> q = new LinkedList<>();
 
+        for(int i=0; i<progresses.length; i++) {
+            q.add((int)Math.ceil((100-progresses[i])/speeds[i]));
+        }
 
-        
-        return answer;
+        List<Integer> answer = new ArrayList<>();
+        while(!q.isEmpty()) {
+            int day = q.poll();
+            int cnt = 1;
+            while(!q.isEmpty() && day >= q.peek()) {
+                cnt++;
+                q.poll();
+            }
+            answer.add(cnt);
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
     
     public static void main(String ars[]) {
