460b4ae35ede5520267a009a54916c3b3c23124e
Yee_Dev <y9413sh@gmail.com>
2021-07-23 11:35:23 +0900

    프린터 문제풀이

--- src/programmers/p42586.java
+++ src/programmers/p42586.java
@@ -8,20 +8,20 @@ import java.util.Queue;
 // 문제 출처: 프로그래머스
 // URL: https://programmers.co.kr/learn/courses/30/lessons/42586
 
-public class 기능개발 {
+public class p42586 {
 
     public int[] solution(int[] progresses, int[] speeds) {
         Queue<Integer> q = new LinkedList<>();
 
         for(int i=0; i<progresses.length; i++) {
-            q.add((int)Math.ceil((100-progresses[i])/speeds[i]));
+            q.add((int)Math.ceil((100-progresses[i])/(double)speeds[i]));
         }
 
         List<Integer> answer = new ArrayList<>();
         while(!q.isEmpty()) {
             int day = q.poll();
             int cnt = 1;
-            while(!q.isEmpty() && day >= q.peek()) {
+            while(!q.isEmpty() && (int)day >= (int)q.peek()) {
                 cnt++;
                 q.poll();
             }
@@ -32,6 +32,6 @@ public class 기능개발 {
     }
     
     public static void main(String ars[]) {
-
+        
     }
 }
